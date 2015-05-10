8df48d4ff894eb6ffd68eb39feffed3c359dfe7b
Jonathan Strickland <djanatyn@gmail.com>
2015-05-10 14:04:11 -0700

    add show instance to Timestamp

--- timestamp.hs
+++ timestamp.hs
@@ -3,20 +3,27 @@ import Numeric (readDec)
 import System.Environment (getArgs)
 import Data.List
 
-data Timestamp = In TimeOfDay | Out TimeOfDay deriving (Show, Eq)
+data Timestamp = In TimeOfDay | Out TimeOfDay deriving (Eq)
 type Day = [Timestamp]
 
 instance Ord Timestamp where
-  (In time1) `compare` (In time2) = time1 `compare` time2
-  (In time3) `compare` (Out time4) = time3 `compare` time4
-  (Out time5) `compare` (In time6) = time5 `compare` time6
+  (In time1) `compare` (In time2)   = time1 `compare` time2
+  (In time3) `compare` (Out time4)  = time3 `compare` time4
+  (Out time5) `compare` (In time6)  = time5 `compare` time6
   (Out time7) `compare` (Out time8) = time7 `compare` time8
+
+instance Show Timestamp where
+  show (In time1)  = "In: " ++ (show time1)
+  show (Out time2) = "Out: " ++ (show time2)
 
 -- parse times like "13:45"
 parseStringTime :: String -> TimeOfDay
 parseStringTime t = TimeOfDay h m 0
   where [(h,rest)]  = readDec t
         [(m,rest1)] = readDec (tail rest)
+
+stamp :: Timestamp
+stamp = In (parseStringTime "13:45")
 
 day :: Day
 day = [In (parseStringTime "13:45"), Out (parseStringTime "13:50"), In (parseStringTime "15:00"), Out (parseStringTime "16:00")]
