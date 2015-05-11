8601ce54e376ea285d0f94c1f357d80253b39a72
Jonathan Strickland <djanatyn@gmail.com>
2015-05-11 12:48:35 -0700

    reformat

--- timestamp.hs
+++ timestamp.hs
@@ -10,6 +10,9 @@ type Day = [Timestamp]
 instance Ord Timestamp where
   (Stamp timeIn1 _) `compare` (Stamp timeIn2 _) = timeIn1 `compare` timeIn2
 
+instance Show Timestamp where
+  show (Stamp timeIn timeOut) = (show timeIn) ++ " -> " ++ (show timeOut)
+
 -- parse times like "13:45"
 stringToTime :: String -> TimeOfDay
 stringToTime t = TimeOfDay h m 0
@@ -17,15 +20,10 @@ stringToTime t = TimeOfDay h m 0
         [(m,rest1)] = readDec (tail rest)
 
 stamp1 :: Timestamp
-stamp1 = Stamp { timeIn  = stringToTime "13:30"
-               , timeOut = stringToTime "13:50" }
+stamp1 = Stamp { timeIn = stringToTime "13:30" , timeOut = stringToTime "13:50" }
 
 stamp2 :: Timestamp
-stamp2 = Stamp { timeIn  = stringToTime "14:00"
-               , timeOut = stringToTime "15:00" }
+stamp2 = Stamp { timeIn = stringToTime "14:00" , timeOut = stringToTime "15:00" }
 
 day :: Day
 day = [stamp1, stamp2]
-
-instance Show Timestamp where
-  show (Stamp timeIn timeOut) = (show timeIn) ++ " -> " ++ (show timeOut)
